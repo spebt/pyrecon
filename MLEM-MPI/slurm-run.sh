29e1d793d410280deaac112bc23b9d80677e3624
Fang Han <fhan0904@gmail.com>
2023-12-04 14:22:59 -0500

    Add more phantoms, tested on the CCR cluster

--- MLEM-MPI/slurm-run.sh
+++ MLEM-MPI/slurm-run.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=mpi-recon
 #SBATCH --output=mpi-recon.out
 #SBATCH --time=00:10:00
-#SBATCH --nodes=4
+#SBATCH --nodes=7
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=general-compute
 #SBATCH --qos=nih
@@ -13,5 +13,5 @@ export I_MPI_FABRICS_LIST=tcp
 export I_MPI_DEBUG=0
 export I_MPI_PMI_LIBRARY=/usr/lib64/libpmi.so
 module load intel-mpi
-# srun -n $SLURM_NPROCS python MLEM-mpi.py
-srun -n 50 python MLEM-mpi.py
+# srun -n $SLURM_NPROCS python MLEM-mpi.py 
+srun -n 100 python mlem-mpi.py config.yml
